0e6eba19f9cfaa5772554863773f97b44f8a0343
sowjanyapullabhatla <Sowjanyakruthii@gmail.com>
2025-10-09 17:30:44 -0500

    Natural numer addition

---
+++ Csharp.Arrays/Naturaladd.cs
@@ -0,0 +1,11 @@
+﻿namespace SumOfNaturalNumbers
+{
+    public class SumCalculator
+    {
+        // Method renamed to NaturalAdd
+        public int NaturalAdd(int n)
+        {
+            return n * (n + 1) / 2;
+        }
+    }
+}
---
+++ Csharp.Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Please enter a number greater than zero.");
+                return;
+            }
+
+            // Create object of SumCalculator class
+            SumCalculator calculator = new SumCalculator();
+
+            // Call the renamed method
+            int sum = calculator.NaturalAdd(n);
+
+            Console.WriteLine($"\nSum of first {n} natural numbers is: {sum}");
+        }
+    }
+}
